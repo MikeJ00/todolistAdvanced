e1763b5b5b328349a9636e70effc1128283ab91b
MikeJ0 <misha7709123@gmail.com>
2024-04-28 22:01:44 +0300

    closed for 3 weeks

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,4 @@
-import React, {Fragment, useRef, useState} from 'react';
+import React, {ChangeEvent, KeyboardEvent, Fragment, useRef, useState} from 'react';
 import {FilterValues, TasksProps} from "./App";
 import {Button} from "./Button";
 
@@ -16,12 +16,27 @@ export const Todolist = ({title, tasks, removeTaskCb, changeFilterCB, addTaskCB}
     addTaskCB(taskTitle)
     setTaskTitle("")
   }
+  const changeTaskTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setTaskTitle(e.currentTarget.value)
+  }
+  const addTaskOnKeyUpHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      addTaskHandler()
+    }
+  }
+  const changeFilterHandlerCreator = (filer: FilterValues) => {
+    return () => {
+      changeFilterCB(filer)
+    }
+  }
+  // const changeFilterHandlerCreator = (filer: FilterValues) => () => {changeFilterCB(filer)}
   return (
     <div>
       <h3>{title}</h3>
       <div>
         <input value={taskTitle}
-               onChange={(e) => setTaskTitle(e.currentTarget.value)}/>
+               onChange={changeTaskTitleHandler}
+               onKeyUp={addTaskOnKeyUpHandler}/>
         <Button title={"+"}
                 onClick={addTaskHandler}/>
       </div>
@@ -30,21 +45,24 @@ export const Todolist = ({title, tasks, removeTaskCb, changeFilterCB, addTaskCB}
       ) : (
         <ul>
           {tasks.map((el) => {
+            const removeTaskHandler = () => {
+              removeTaskCb(el.id)
+            }
             return (
               <li key={el.id}>
                 <input type="checkbox" checked={el.isDone}/>
                 <span>{el.title}</span>
                 <Button title={"X"}
-                        onClick={() => removeTaskCb(el.id)}/>
+                        onClick={removeTaskHandler}/>
               </li>
             )
           })}
         </ul>
       )}
       <>
-        <Button title={"All"} onClick={() => changeFilterCB("all")}/>
-        <Button title={"Active"} onClick={() => changeFilterCB("active")}/>
-        <Button title={"Completed"} onClick={() => changeFilterCB("completed")}/>
+        <Button title={"All"} onClick={changeFilterHandlerCreator("all")}/>
+        <Button title={"Active"} onClick={changeFilterHandlerCreator("active")}/>
+        <Button title={"Completed"} onClick={changeFilterHandlerCreator("completed")}/>
       </>
     </div>
   );
